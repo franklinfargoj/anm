9f04ca0787cbab93b8632fc55b3772e5c4eba12a
Franklin <franklin.fargoj@neosofttech.com>
2018-07-06 22:29:19 +0530

    View page for scenrio 1

--- resources/views/scenerio/scenerio_1.blade.php
+++ resources/views/scenerio/scenerio_1.blade.php
@@ -1,7 +1,102 @@
 <html>
 
-<h3>Scenrio 1</h3>
+<head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style type="text/css">
+        body {
+            margin: 10px;
+            padding: 0;
+        }
+    </style>
+</head>
 
-{{ dd($lstAnmCategory) }}
+<body>
+<table width="540" cellpadding="0" cellspacing="0" border="0" align="center" style="font-family: Arial; background-color: #f0c94a; padding: 10px;">
+    <tr>
+        <td>
+            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="padding-bottom: 15px;">
+                <tr>
+                    <td style="font-size: 24px; font-weight: bold; line-height: 32px; text-align: center;">जानना चाहते है की <span style="color: #ec1d25;">मई <span style="text-decoration: underline;">2018</span></span> में <span style="color: #ec1d25;">कुचील पी.एच.सी</span> <br/> के किस <span style="color: #ec1d25; font-size: 30px;">ANM</span> ने सबसे अच्छा काम किया?</td>
+                </tr>
+            </table>
 
+            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color: #ffffff; padding: 10px 25px;">
+                <tr>
+                    <td style="border-top: solid 1px #08683a; border-bottom: solid 1px #08683a; padding: 10px 0px;">
+                        <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                            <tr>
+                                <td align="left"><img src="http://design.neosofttech.in/50/rajesh/BCG/html/images/newsletter-img/announcment-icon-left.jpg" alt="" style="margin-left: -20px;"></td>
+                                <td style="font-size: 36px; line-height: 36px; color: #08683a; font-weight: bold; text-align: center;">शाबाश ANM दीदी </td>
+                                <td align="right"><img src="http://design.neosofttech.in/50/rajesh/BCG/html/images/newsletter-img/announcment-icon-right.jpg" alt="" style="margin-right: -20px;"></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color: #ffffff;">
+                <tr>
+                    <td align="left" valign="middle"><img src="http://design.neosofttech.in/50/rajesh/BCG/html/images/newsletter-img/pic1-left.jpg" alt="" alt=""></td>
+                    <td align="center" style="font-weight: bold;">
+
+                        <span style="color: #ec1d25; font-size: 30px;"> {{ $top_names_except_last }}<span>
+                                और
+						<span style="color: #ec1d25; font-size: 30px;"> {{ $top_names_last }}<span>
+
+						<p style="color: #000; font-size: 18px; line-height: 30px; margin:0;">आपने मार्च में अव्वल दर्जे का काम कर दिखाया! आप में से कुछ <span style="color: #ec1d25; font-size: 24px;">ANMs</span> ने 80% बच्चो का टीकाकरण पूरा कर दिखाया तथा कुछ ने 30% गर्भवती महिलाओ का चौथा ANC चैकप भी पूरा किया!</p>
+                    </td>
+                </tr>
+            </table>
+
+            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color: #ffffff; padding-top: 30px;">
+                <tr>
+                    <td align="center" style="font-weight: bold;">
+
+
+                        <span style="color: #ec1d25; font-size: 30px;">
+                            <span style="font-size:20px; color: #000; display: block;">हमारी</span>
+                            {{ $middle_names_except_last }}
+
+                            एवं
+
+                           {{ $middle_names_last }}
+                        <span>
+
+                        <p style="color: #000; font-size: 18px; line-height: 30px; margin:0;">दीदी ने भी अच्छा करने प्रयास किया 30% - 80% बच्चो का टीकाकरण कर दिखाया साथ ही साथ 10% - 30% गर्भवती महिलाओ का चौथा ANC चैकप भी पूरा किया!</p>
+                    </td>
+                    <td align="right" valign="middle"><img src="http://design.neosofttech.in/50/rajesh/BCG/html/images/newsletter-img/pic2-right.jpg" alt="" alt=""></td>
+                </tr>
+            </table>
+
+            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color: #ffffff; padding-top: 30px;">
+                <tr>
+                    <td align="left" valign="middle"><img src="http://design.neosofttech.in/50/rajesh/BCG/html/images/newsletter-img/pic3-left.jpg" alt="" alt=""></td>
+                    <td align="center" style="font-weight: bold;">
+						<span style="color: #ec1d25; font-size: 30px;">
+
+                         {{ $bottom_names_last }}
+
+                         <span>
+						<p style="color: #000; font-size: 18px; line-height: 30px; margin:0;">आपको भी 80% बच्चो का टीकाकरण एवं 30% से भी अधिक गर्भवती महिलाओ का चौथा ANC करके दिखाना है| थोड़ी और तैयारी करो| </p>
+                    </td>
+                </tr>
+            </table>
+
+            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color: #ffffff; padding: 25px 25px 5px;">
+                <tr>
+                    <td align="center" colspan="2" style="font-size: 18px; line-height: 30px; font-weight: bold; border-top: solid 1px #000;">
+                        बने रहिये: देखते है जून 2018 में कौनसी <span style="color: #ec1d25; font-size: 26px;">ANM</span> अव्वल नंबर का काम करके दिखाएगी|
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align="center" colspan="2" style="">
+
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
 </html>
