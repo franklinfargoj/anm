c2fe6612193661777249b8dc4c82d0aadbb3b853
Franklin <franklin.fargoj@neosofttech.com>
2018-07-06 16:48:11 +0530

    Added weblink route

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,18 @@ Route::group(['middleware'=>'auth'], function (){
 
     Route::get('/', 'TargetdataController@index')->name('dashboard');
     Route::post('import-file', 'TargetdataController@importFile')->name('import.file');
-
     Route::get('/get-anm-target-data', 'TargetdataController@fetchTargetData');
-
     Route::get('/fetch-process-data/{id}', 'ProcessedFileController@fetchProcessData');
-
-
     Route::get('processedfile/{id}','ProcessedFileController@index')->name('processedfile');
 
 
-   // Route::get('/home', 'TargetdataController@homePage')->name('home');
+    Route::get('weblink/{id}','WeblinkController@index')->name('weblink');
+
+    // Route::get('/home', 'TargetdataController@homePage')->name('home');
     //Route::get('/home', 'HomeController@index')->name('home');
     //Route::post('register', 'Controller@importFile')->name('register');
     //Route::get('login', 'LoginController@login')->name('login');
     // Route::get('/', function () { return view('fileupload'); });
-
-
 });
 
 
