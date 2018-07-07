bf3179a07b19fc04a7e26cbcefaed8c0db5e94ab
Franklin <franklin.fargoj@neosofttech.com>
2018-07-07 19:59:19 +0530

    Route file commit error

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,10 @@ Route::group(['middleware'=>'auth'], function (){
     Route::get('/get-anm-target-data', 'TargetdataController@fetchTargetData');
     Route::get('/fetch-process-data/{id}', 'ProcessedFileController@fetchProcessData');
     Route::get('processedfile/{id}','ProcessedFileController@index')->name('processedfile');
-
-
     Route::get('weblink/{id}','WeblinkController@index')->name('weblink');
-
     // Route::get('/home', 'TargetdataController@homePage')->name('home');
     //Route::get('/home', 'HomeController@index')->name('home');
     //Route::post('register', 'Controller@importFile')->name('register');
-    //Route::get('login', 'LoginController@login')->name('login');
     // Route::get('/', function () { return view('fileupload'); });
 });
 
