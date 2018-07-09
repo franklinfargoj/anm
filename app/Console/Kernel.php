b72d305b2291a68969e2ae9ad172855bb1243b3b
abhijeet9920 <abhijeet.umbarkarcmpn2@gmail.com>
2018-07-09 13:26:53 +0530

    Setting up console command to run every 5 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('custom:command')->daily();
+        $schedule->command('custom:command')->everyFiveMinutes();
     }
 
     /**
