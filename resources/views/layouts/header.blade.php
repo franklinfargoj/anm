41621ecc0cbf41adb0194d89417bcd53cdc68327
Franklin <franklin.fargoj@neosofttech.com>
2018-07-13 16:22:52 +0530

    Replaced name of Anm and moic

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,8 +6,8 @@
             </div>
             <div class="col-md-6 text-right header-links">
 
-                        <a href="{{url('/')}}">ANM Import</a>
-                        <a href="{{url('/mos')}}">MOIC Ranking Import</a>
+                        <a href="{{url('/')}}">ANM Performance</a>
+                        <a href="{{url('/mos')}}">MOIC Performance</a>
                         <a class="logout-btn" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             <span class="glyphicon glyphicon-log-out"></span> Logout
                         </a>
