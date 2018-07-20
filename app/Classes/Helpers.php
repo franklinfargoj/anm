d3a6ae4fd9ae119e725a6316f7e9c5cc3c712298
Franklin <franklin.fargoj@neosofttech.com>
2018-07-20 16:19:40 +0530

    SMS in helper

--- app/Classes/Helpers.php
+++ app/Classes/Helpers.php
@@ -27,6 +27,66 @@ class Helpers{
 	public static function sendSms($sms, $mobile)
 	{
 		return ["status" => true, "message" => "sms sent successfully"];
+
+        $username="franklin";                      //username of the department
+        $password="franklinf123";                  //password of the department
+        $encryp_password=sha1(trim($password));
+        $senderid="1";                             //senderid of the deparment
+        $message="Your Normal message here ";      //message content
+        $mobileno="9561459348";                    //if single sms need to be send use mobileno keyword
+        $deptSecureKey= "11";                      //departsecure key for encryption of message...
+
+        sendSingleSMS($username,$encryp_password,$senderid,$message,$mobileno,$deptSecureKey);
+
+        //Function to send single sms
+        function sendSingleSMS($username,$encryp_password,$senderid,$message,$mobileno,$deptSecureKey){
+            $key=hash('sha512',trim($username).trim($senderid).trim($message).trim($deptSecureKey));
+            $data = array(
+                "username" => trim($username),
+                "password" => trim($encryp_password),
+                "senderid" => trim($senderid),
+                "content" => trim($message),
+                "smsservicetype" =>"singlemsg",
+                "mobileno" =>trim($mobileno),
+                "key" => trim($key)
+            );
+            post_to_url("https://msdgweb.mgov.gov.in/esms/sendsmsrequest",$data); //calling post_to_url to send sms
+        }
+
+        //function to send bulk sms
+        function sendBulkSMS($username,$encryp_password,$senderid,$message,$mobileNos,$deptSecureKey){
+            $key=hash('sha512', trim($username).trim($senderid).trim($message).trim($deptSecureKey));
+
+            $data = array(
+                "username" => trim($username),
+                "password" => trim($encryp_password),
+                "senderid" => trim($senderid),
+                "content" => trim($message),
+                "smsservicetype" =>"bulkmsg",
+                "bulkmobno" =>trim($mobileNos),
+                "key" => trim($key)
+            );
+            post_to_url("https://msdgweb.mgov.gov.in/esms/sendsmsrequest",$data); //calling post_to_url to send bulk sms
+        }
+
+        function post_to_url($url, $data) {
+            $fields = '';
+            foreach($data as $key => $value) {
+                $fields .= $key . '=' . urlencode($value) . '&';
+            }
+            rtrim($fields, '&');
+            $post = curl_init();
+            //curl_setopt($post, CURLOPT_SSLVERSION, 5); // uncomment for systems supporting TLSv1.1 only
+            curl_setopt($post, CURLOPT_SSLVERSION, 6); // use for systems supporting TLSv1.2 or comment the line
+            curl_setopt($post,CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($post, CURLOPT_URL, $url);
+            curl_setopt($post, CURLOPT_POST, count($data));
+            curl_setopt($post, CURLOPT_POSTFIELDS, $fields);
+            curl_setopt($post, CURLOPT_RETURNTRANSFER, 1);
+            $result = curl_exec($post); //result from mobile seva server
+            echo $result; //output from server displayed
+            curl_close($post);
+        }
 	}
 
 	public static function ordinal($number) {
