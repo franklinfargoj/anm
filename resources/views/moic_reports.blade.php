677c049866a8967093630f1f012dac4033fe5d9c
webwerk <martin.philipose@wwindia.com>
2018-07-27 14:12:40 +0530

    Responsinve ui changes

--- resources/views/moic_reports.blade.php
+++ resources/views/moic_reports.blade.php
@@ -3,10 +3,10 @@
    <head>
       <title></title>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!--       <meta name="viewport" content="width=device-width, initial-scale=1.0" /> -->
       <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
       <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-      <link rel="stylesheet" type="text/css" href="{{ asset('css/responsive.css') }}">
+      <!-- <link rel="stylesheet" type="text/css" href="{{ asset('css/responsive.css') }}"> -->
    </head>
    <body class="internal-page">
       <header class="internal-header navbar-fixed-top">
