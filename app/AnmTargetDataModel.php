746e3934d3c4b49a2ffd056eda02bc994653de48
Franklin <franklin.fargoj@neosofttech.com>
2018-10-04 15:52:01 +0530

    ANM and MOIC model changes for softdelete

--- app/AnmTargetDataModel.php
+++ app/AnmTargetDataModel.php
@@ -3,11 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AnmTargetDataModel extends Model
 {
+    use SoftDeletes;
     protected $table = "anm_target_data";
     protected $primaryKey = 'id';
+    protected $dates = ['deleted_at'];
     public $timestamps = false;
 
     public function block(){
