70f3ff6593212fffd5048fc016a532b35ad6df7a
Franklin <franklin.fargoj@neosofttech.com>
2018-10-15 11:56:41 +0530

    Highlight the selected module

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,19 +4,18 @@
             <div class="col-md-6">
                 <div class="row">
                     <div class="col-md-3">
-                        <img src="{{ asset('images/logo.png') }}" alt="" class="img-responsive">
+                        <a href="{{ url('/') }}"><img src="{{ asset('images/logo.png') }}" alt="" class="img-responsive"></a>
                     </div>
                     <div class="col-md-9">
-                        <div class="tagline">Governance <span>Innovation Model</span></div>
+                        <a href="{{ url('/') }}"><div class="tagline">Governance <span>Innovation Model</span></div></a>
                     </div>
                 </div>
 
             </div>
             <div class="col-md-6 text-right header-links">
-
-                        <a href="{{url('/')}}">ANM Performance</a>
-                        <a href="{{url('/get-mos')}}">MOIC Performance</a>
-                        <a href="{{url('/feedback')}}">Patient Feedback</a>
+                        <a class="{{ (\Request::is('get-anm') || \Request::is('processedfile/*')) ? "active" : ''}}" href="{{url('get-anm')}}">ANM Performance</a>
+                        <a class="{{ (\Request::is('get-mos') || \Request::is('rankingdetails/*'))? "active" : ''}}"  href="{{url('/get-mos')}}">MOIC Performance</a>
+                        <a class="{{ (\Request::is('feedback')|| \Request::is('detail_feedback/*')) ? "active" : ''}}" href="{{url('/feedback')}}">Patient Feedback</a>
                         <a class="logout-btn" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             <span class="glyphicon glyphicon-log-out"></span> Logout
                         </a>
@@ -24,7 +23,6 @@
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             {{ csrf_field() }}
                         </form>
-
             </div>
         </div>
     </div>
