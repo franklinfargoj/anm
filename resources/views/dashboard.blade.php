ab6f9323ab6c3f4859db7302d0931f0e6c22b291
Franklin <franklin.fargoj@neosofttech.com>
2018-12-27 12:17:09 +0530

    added nudge in dshboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -83,6 +83,8 @@
                                 <h3>Moic files</h3>
                             @elseif($category=="Feedback")
                                 <h3>Feedback files</h3>
+                            @elseif($category=="Nudges")
+                                <h3>Nudge files</h3>
                             @endif
 
                     <table id="uploadData" class="table table-bordered table-striped">
@@ -93,14 +95,20 @@
                             <th>Uploaded On</th>
                             <th>Total rows</th>
                             <th>Number of sms sent</th>
+
+                            @if($category != "Nudges")
                             <th>Weblinks Opened</th>
+                            @endif
+
                             <th>Action</th>
 
+                            @if($category != "Nudges")
+                            <th>View graph</th>
+                            @endif
                         </tr>
                         </thead>
 
                         @if($list_data)
-                        @php $i = 1;@endphp
                         @php $i = 1;@endphp
                         @foreach ($list_data as $key => $value)
                         <tr>
@@ -109,9 +117,15 @@
                             <td>{{ $value['uploaded_on'] }}</td>
                             <td>{{ $value['total_rows'] }}</td>
                             <td>{{ $value['countSentSms'] }}</td>
+
+                            @if(isset($value['weblink_opened']))
                             <td>{{ $value['weblink_opened'] }}</td>
+                            @endif
+
                             <td> <a href="{{ url($category.'/'.$value['id']) }}">View details</a></td>
-                           
+                            @if($category != "Nudges")
+                            <td> <a href="{{ url($category.'graph/'.$value['id']) }}">Graph view</a></td>
+                            @endif
                         </tr>
                         @endforeach
                         @endif
